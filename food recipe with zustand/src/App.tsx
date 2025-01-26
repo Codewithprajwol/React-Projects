3baca1069bff1441d4bfad468b8cb01fcfd588b3
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-26 18:17:39 +0545

    finish: crud operation on food recipe

--- food recipe with zustand/src/App.tsx
+++ food recipe with zustand/src/App.tsx
@@ -38,13 +38,33 @@ const App: FC = (): ReactElement => {
       instructions.trim() === ""
     )
       return;
+      if(editing){
+        removeRecipe(editing.id)
+
+        addRecipe({id:Date.now(),name,ingredients:ingredients.split(',').map((ingredients)=>ingredients.trim()),instructions})
+        setEditing(null)
+      }
+      setName('')
+      setIngredients('')
+      setInstructions('')
+  }
+
+  const cancelEditHandle=()=>{
+    setEditing(null)
+    setName('')
+    setIngredients('')
+    setInstructions('')
+  }
+  const HandleDeleteRecipe=(recipe:Recipe)=>{
+    if(editing) return
+    removeRecipe(recipe.id)
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-green-400">
+    <div className="min-h-screen flex items-center justify-center bg-green-400 pt-5">
       <div className="p-6 rounded-lg w-full max-w-2xl bg-white shadow-lg">
         <h1 className="text-3xl font-bold mb-6 text-center text-green-300">
-          Recipe App
+          Vegeterian Food Recipe
         </h1>
         <div className="space-y-4 mb-6">
           <input
@@ -68,42 +88,43 @@ const App: FC = (): ReactElement => {
             placeholder="instructions"
           />
           {editing ? (
-            <>
+            <div className="flex items-center justify-between">
               <button
                 className="px-4 py-2 bg-amber-400 rounded-lg text-white focus:bg-amber-600 focus:ring-2 focus:ring-yellow-900 cursor-pointer"
                 onClick={handleUpdateRecipe}
               >
                 update
               </button>
-              <button className="Px-4 py-2 bg-red-400 rounded-lg text-white focus:bg-red-600 focus:ring-2 focus:ring-red-900 cursor-pointer">
+              <button className="px-4 py-2 bg-red-400 rounded-lg text-white focus:bg-red-600 focus:ring-2 focus:ring-red-900 cursor-pointer" onClick={cancelEditHandle}>
                 Cancel
               </button>
-            </>
+            </div>
           ) : (
             <>
               <button
                 className="px-4 py-2 bg-green-400 rounded-lg text-white focus:bg-green-600 focus:ring-2 focus:ring-green-900 cursor-pointer"
                 onClick={handleAddRecipe}
               >
-                Add
+                Add Recipe
               </button>
             </>
           )}
         </div>
         <div>
-          <ul className="space-y-4">
+          <ul className="flex item-center justify-center flex-col-reverse gap-5 ">
             {recipes.map((recipe) => (
               <li
                 key={recipe.id}
                 className="p-4 bg-green-50 rounded-lg shadow-sm"
               >
-                <h2 className="text-xl font-semibold text-green-800 mb-2">
+                <h2 className="text-xl font-semibold text-green-800 mb-1 ">
                   {recipe.name}
                 </h2>
-                <p className="text-gray-700 mb-2">
+                <p className="text-gray-700 mb-1">
                   <strong>Ingredients:</strong>
-                  {recipe.ingredients.join(",")}
+                  {recipe.ingredients.join(", ")}
                 </p>
+                <p className="text-gray-700 mb-2">{recipe.instructions}</p>
                 <div className="flex items-center justify-between">
                   <button
                     className="px-4 py-2 bg-amber-400 rounded-lg text-white focus:bg-amber-600 focus:ring-2 focus:ring-yellow-900 cursor-pointer"
@@ -114,7 +135,7 @@ const App: FC = (): ReactElement => {
 
                   <button
                     className="px-4 py-2 bg-red-400 rounded-lg text-white focus:bg-red-600 focus:ring-2 focus:ring-red-900 cursor-pointer"
-                    onClick={() => removeRecipe(recipe.id)}
+                    onClick={() => HandleDeleteRecipe(recipe)}
                   >
                     Delete
                   </button>
